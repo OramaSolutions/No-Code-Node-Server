a56bd9ba2fcd64447a907bbe6b5877c791f76711
adyeetya <2612adityasingh2000@gmail.com>
2025-12-22 16:02:46 +0530

    fix in refresh controller

--- controller/user/refreshController.js
+++ controller/user/refreshController.js
@@ -5,17 +5,19 @@ const { createAccessToken, createRefreshToken } = require('../../auth/tokens');
 const refresh = async (req, res) => {
     const token = req.cookies.refresh_token;
     if (!token) return res.sendStatus(401);
-    // console.log("REFRESH TOKEN>>>", token)
+    console.log("REFRESH TOKEN>>>", token)
 
     let payload;
     try {
         payload = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
     } catch {
+        console.log('payload mismatch 1')
         return res.sendStatus(401);
     }
 
     const user = await User.findById(payload.user_id);
     if (!user || user.tokenVersion !== payload.tokenVersion) {
+        console.log('no user or no token versin match',user, payload.tokenVersion)
         return res.sendStatus(401);
     }
 
@@ -28,15 +30,15 @@ const refresh = async (req, res) => {
 
     res.cookie("access_token", newAccess, {
         httpOnly: true,
-        sameSite: "Lax",
-        secure: process.env.NODE_ENV === "production",
+        sameSite: "None",
+        secure: true,
         maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refresh_token", newRefresh, {
         httpOnly: true,
-        sameSite: "Lax",
-        secure: process.env.NODE_ENV === "production",
+        sameSite: "None",
+        secure: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
         path: "/api/v1/user/refresh",
     });

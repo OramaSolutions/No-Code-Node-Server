1cbb8810a865dc80fd70ccdeeb489dfeed6e3ff1
adyeetya <2612adityasingh2000@gmail.com>
2025-12-22 15:07:41 +0530

    token.js in cjs fix

--- auth/tokens.js
+++ auth/tokens.js
@@ -1,15 +1,20 @@
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
-export const createAccessToken = (userId) =>
+const createAccessToken = (userId) =>
   jwt.sign(
     { user_id: userId },
     process.env.ACCESS_TOKEN_SECRET,
     { expiresIn: "15m" }
   );
 
-export const createRefreshToken = (userId, tokenVersion) =>
+const createRefreshToken = (userId, tokenVersion) =>
   jwt.sign(
     { user_id: userId, tokenVersion },
     process.env.REFRESH_TOKEN_SECRET,
     { expiresIn: "30d" }
   );
+
+module.exports = {
+  createAccessToken,
+  createRefreshToken,
+};
